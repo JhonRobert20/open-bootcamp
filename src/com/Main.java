b0f29e2c0ec24163a3024a4c99cd3ff7fd7f3899
Jhon Robert <jhonbara51214@gmail.com>
2022-07-10 13:58:48 +0200

    ejercicios tema 4

--- src/com/Main.java
+++ src/com/Main.java
@@ -1,7 +1,38 @@
 package com;
 
 public class Main {
+
+    static boolean checkPositive(int number) {
+        return (number > 0);
+    }
+
     public static void main(String[] args) {
+        System.out.println("1, should be true");
+        System.out.println(checkPositive(1));
+
+        System.out.println("-1, should be false");
+        System.out.println(checkPositive(-1));
+
+        int numberWhile = 0;
+
+        System.out.println("While");
+        while (numberWhile < 3) {
+            System.out.println(numberWhile);
+            numberWhile += 1;
+        }
+
+        System.out.println("Do While");
+        int numberDoWhile = 2;
+
+        do {
+            System.out.println(numberDoWhile);
+            numberDoWhile += 1;
+        }while (numberDoWhile < 3);
+
+        System.out.println("For");
+        for (int numberFor=0; numberFor < 3; numberFor++) {
+            System.out.println(numberFor);
+        }
 
     }
 }
